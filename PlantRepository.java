3bc02c7d0b5c311055129682bbb6d3177c3e7322
Archie Citra Muhammad <108534539+archie-cm@users.noreply.github.com>
2024-02-19 16:32:30 +0700

    add custom query

--- PlantRepository.java
+++ PlantRepository.java
@@ -7,4 +7,9 @@ import org.springframework.data.repository.CrudRepository;
 import com.codecademy.plants.entities.Plant;
 
 public interface PlantRepository extends CrudRepository<Plant, Integer> {
+  List<Plant> findByHasFruitTrue();
+  List<Plant> findByHasFruitFalse();
+  List<Plant> findByQuantityLessThan(Integer quantity);
+  List<Plant> findByHasFruitTrueAndQuantityLessThan(Integer quantity);
+  List<Plant> findByHasFruitFalseAndQuantityLessThan(Integer quantity);
 }
